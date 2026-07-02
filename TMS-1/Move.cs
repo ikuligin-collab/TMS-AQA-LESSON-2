1730e48a778c30538ed9d61c4413d35c513fb25e
ikuligin-collab <i.kuligin@evernode.tech>
2026-07-02 19:09:38 +0200

    В ход добавлен диапазон значений и валидация

--- TMS-1/Move.cs
+++ TMS-1/Move.cs
@@ -2,5 +2,25 @@
 
 public class Move
 {
-    
+    public int Value { get; }
+
+    public Move(int value)
+    {
+        Value = value;
+    }
+
+    // Проверка диапазона с параметрами по умолчанию
+    public bool IsValid(int min = 1, int max = 3)
+    {
+        return Value >= min && Value <= max;
+    }
+
+    // Name через switch expression
+    public string Name => Value switch
+    {
+        1 => "Камень",
+        2 => "Бумага",
+        3 => "Ножницы",
+        _ => "Неизвестный ход"
+    };
 }
