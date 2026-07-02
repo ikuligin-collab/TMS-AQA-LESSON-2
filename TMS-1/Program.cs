278fc62766ff376d570bfe18dca95836b6ffd469
Kuligin <i.kuligin@evernode.tech>
2026-07-02 16:54:14 +0200

    Добавленн ввод имени игрока

--- TMS-1/Program.cs
+++ TMS-1/Program.cs
@@ -1,8 +1,35 @@
-﻿using TMS_1;
+﻿using System.Text;
+using TMS_1;
+Console.OutputEncoding = Encoding.UTF8; 
+Console.InputEncoding = Encoding.UTF8;
+Console.WriteLine("=== ДОБРО ПОЖАЛОВАТЬ В ИГРУ ===");
 
-var game = new Game(); // object of type (class) Game
+// Ввод имени игрока
+Console.Write("Введите ваше имя: ");
+string userName = Console.ReadLine();
 
-game.RoundsToPlay = 5;
+// Если пользователь ничего не ввел, даем дефолтное имя
+if (string.IsNullOrWhiteSpace(userName))
+{
+    userName = "Игрок";
+}
+
+//Console.Write(userName);
+// Запрашиваем количество раундов
+Console.Write("Сколько раундов хотите сыграть? ");
+if (!int.TryParse(Console.ReadLine(), out int rounds) || rounds <= 0 || rounds >= 100)
+{
+    Console.WriteLine("Некорректный ввод. Установлено стандартное количество раундов: 3");
+    rounds = 3;
+}
+
+Console.Clear(); // Очищаем консоль перед началом игры
+
+// Создаём объекты игроков
+Player human = new Player(userName);
+Player computer = new Player("Компьютер");
+
+Game game = new Game(human, computer, rounds);
 game.Play();
 
 if (game.UserWon)
