3ec1ac0166544e142c7807027e8ac757723eae0b
ikuligin-collab <i.kuligin@evernode.tech>
2026-07-02 20:19:42 +0200

    Плеер

--- TMS-1/Player.cs
+++ TMS-1/Player.cs
@@ -2,5 +2,13 @@
 
 public class Player
 {
-    
+    public string Name { get; }
+    public int Score { get; set; }
+
+    // Пункт 1: Конструктор принимает имя
+    public Player(string name)
+    {
+        Name = name;
+        Score = 0;
+    }
 }
