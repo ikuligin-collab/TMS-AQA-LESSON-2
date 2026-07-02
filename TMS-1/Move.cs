8dfd141314973fd8958b1e517ead362a6a1a9647
ikuligin-collab <i.kuligin@evernode.tech>
2026-07-02 20:17:53 +0200

    Мув

--- TMS-1/Move.cs
+++ TMS-1/Move.cs
@@ -2,5 +2,25 @@
 
 public class Move
 {
-    
+    public int Value { get; }
+
+    public Move(int value)
+    {
+        Value = value;
+    }
+
+    // Пункт 2: Проверка диапазона с параметрами по умолчанию
+    public bool IsValid(int min = 1, int max = 3)
+    {
+        return Value >= min && Value <= max;
+    }
+
+    // Пункт 4: Вычисляемое свойство Name через switch expression
+    public string Name => Value switch
+    {
+        1 => "Камень",
+        2 => "Бумага",
+        3 => "Ножницы",
+        _ => "Неизвестный ход"
+    };
 }
