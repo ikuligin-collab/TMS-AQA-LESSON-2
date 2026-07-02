0477db2a301d7679dc9e17d3d322ba8148dcd047
ikuligin-collab <i.kuligin@evernode.tech>
2026-07-02 20:21:16 +0200

    програм

--- TMS-1/Program.cs
+++ TMS-1/Program.cs
@@ -1,15 +1,34 @@
-﻿using TMS_1;
+﻿using System.Text;
+using TMS_1;
 
-var game = new Game(); // object of type (class) Game
+Console.OutputEncoding = Encoding.UTF8; 
+Console.InputEncoding = Encoding.UTF8;
 
-game.RoundsToPlay = 5;
+Console.WriteLine("=== ДОБРО ПОЖАЛОВАТЬ В ИГРУ ===");
+
+// Ввод имени игрока
+Console.Write("Введите ваше имя: ");
+string userName = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(userName))
+{
+    userName = "Игрок";
+}
+
+// Запрашиваем количество раундов
+Console.Write("Сколько раундов хотите сыграть? ");
+if (!int.TryParse(Console.ReadLine(), out int rounds) || rounds <= 0 || rounds >= 100)
+{
+    Console.WriteLine("Некорректный ввод. Установлено стандартное количество раундов: 3");
+    rounds = 3;
+}
+
+Console.Clear();
+
+// Создаём объекты игроков
+Player human = new Player(userName);
+Player computer = new Player("Компьютер");
+
+// Конструирование и запуск игры (всё строго по условию)
+Game game = new Game(human, computer, rounds);
 game.Play();
-
-if (game.UserWon)
-{
-    Console.WriteLine("You won");
-}
-else
-{
-    Console.WriteLine("You lost");
-}
